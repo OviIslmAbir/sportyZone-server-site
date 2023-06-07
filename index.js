7ff6fe7eb9b1a722a920b151eded55f803b5e249
Abir <abirovi481@gmail.com>
2023-06-07 17:31:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// middleware
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.maovuz9.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    client.connect();
+
+
+
+    
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.log);
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('SportyZone is running')
+})
+
+app.listen(port, () => {
+  console.log(`SportyZone is running on port ${port}`)
+})
