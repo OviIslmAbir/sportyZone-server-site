2d39213450ef883acb95e20fc86fa0094a8ac35f
Abir <abirovi481@gmail.com>
2023-06-09 15:11:42 +0600

    5th commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
@@ -47,6 +47,12 @@ async function run() {
     app.post('/selectedClasses', async(req, res) => {
         const item = req.body
         const result = await selectedClassesCollection.insertOne(item)
+        res.send(result)
+    })
+    app.delete('/selectedClasses/:id', async(req, res) => {
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await selectedClassesCollection.deleteOne(query)
         res.send(result)
     })
 
